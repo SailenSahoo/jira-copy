8a0e250975e89bc45a31563d650f7686a4c54f30
Sailen Sahoo <sailensahoo8100@gmail.com>
2025-05-13 14:37:37 +0530

    Update db.py

--- jira_clone_backend_full/db.py
+++ jira_clone_backend_full/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./jira.db"
+DATABASE_URL = "oracle+cx_oracle://<username>:<password>@<hostname>:<port>/<service_name>"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
